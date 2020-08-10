dedd2064b55145e34340dc0c295f43385fdd8d99
Sharon Mansoor <sharonmans@gmail.com>
2020-08-10 16:52:07 +0300

    fix filter hours

--- controllers/getAllAlerts.js
+++ controllers/getAllAlerts.js
@@ -9,24 +9,11 @@ const handleAlerts = (req, res) => {
 
         dbo.collection("CurrentAlerts").find({}).toArray(function(err, result) {
           if (err) throw err;
-         
-          //console.log('year', test,  month, year, day, hour, min, 0)
-
-          //let test2 = new Date(Number(test.substring(6,8)) + 2000, Number(test.substring(3,5)), Number(test.substring(0,2)), Number(test.substring(9,11)), Number(test.substring(11,13)),0,0);
-          //test2 = new Date(2020,06,12,13,40,0,0)
-          
-          //20.06.20 12:00AM
-          //console.log('today' ,today);
-          
-
-          //var today = new Date();
-          //console.log(today);
-         // console.log(result[0].time);
-//          console.log(today > today2);
           const filteredData = result.filter(alert => {
             let today = new Date();
-            today = new Date(today.getFullYear(),today.getMonth(),today.getDate()); 
+            today.setHours(0,0,0,0);
             let alertDate = new Date(alert.time);
+            alertDate.setHours(0,0,0,0);
             return alertDate >= today;
           })
           
